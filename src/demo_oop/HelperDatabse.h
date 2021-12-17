a21ae23411c0f81cd963dc7dc281b50a440e04b4
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2021-12-17 22:59:13 +0700

    add to-do jobs

---
+++ src/demo_oop/HelperDatabse.h
@@ -0,0 +1,32 @@
+// TODO: create HelperDatabase class to store helper informations
+/*
+    Các thông tin 
+        trợ giúp cách đăng kí thi bằng lái xe
+        trợ giúp về cách sử dụng app
+        trợ giúp thi lý thuyết, thực hành
+        trợ giúp tìm kiếm địa điểm thi (only HCM -> chia theo nhiều quận, huyện)
+*/
+// Also to create/propose a way to store database (suggest storing as txt)
+class HelperDatabase{
+    
+};
+
+class TheoryHelper: public HelperDatabase{
+
+};
+
+class PraticeHelper: public HelperDatabase{
+
+};
+
+class RegisterHelper: public HelperDatabase{
+
+};
+
+class AppHelper: public HelperDatabase{
+
+};
+
+class LocateHelper: public HelperDatabase{
+
+};
---
+++ src/demo_oop/User.h
@@ -0,0 +1,14 @@
+// TODO: create User class to store informations of user
+/*
+    Requried attributes:
+    - Username
+    - Password
+    Optional attributes:
+    - Sex
+    - Birth
+    - ...
+*/
+// Also to create/propose a way to store database of users
+class User{
+    
+};
