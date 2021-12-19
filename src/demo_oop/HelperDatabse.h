ae56aa8eeffea11d669a39a32419f9d06c3c2a55
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2021-12-19 16:12:18 +0700

    Update HelperDatabse.h

--- src/demo_oop/HelperDatabse.h
+++ src/demo_oop/HelperDatabse.h
@@ -7,26 +7,94 @@
         trợ giúp tìm kiếm địa điểm thi (only HCM -> chia theo nhiều quận, huyện)
 */
 // Also to create/propose a way to store database (suggest storing as txt)
-class HelperDatabase{
-    
+
+#pragma once
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+
+
+class HelperDatabase {
+protected:
+	vector<string> data;
+	string path;
+public:
+	static void readData(const string& path, vector<string>& data) {
+		if (path.length() > 0) {
+			ifstream fi(path);
+
+			if (fi) {
+				string line;
+				while (getline(fi, line))
+					data.push_back(line);
+
+				fi.close();
+			}
+		}
+	}
+
+	vector<string> getData() {
+		return data;
+	}
 };
 
-class TheoryHelper: public HelperDatabase{
-
+// data[0], data[1], data[2]: A1
+// data[3]: A2
+// data[4]: B2
+class TheoryHelper : public HelperDatabase {
+public:
+	TheoryHelper() {
+		path = "TheoryHelperDatabase.txt";
+		HelperDatabase::readData(path, data);
+	}
 };
 
-class PraticeHelper: public HelperDatabase{
 
+// data[0]: A1 
+// data[1]: A2
+// data[2]: B1
+// data[3]: B2
+class PracticeHelper : public HelperDatabase {
+public:
+	PracticeHelper() {
+		path = "PracticeHelperDatabase.txt";
+		HelperDatabase::readData(path, data);
+	}
 };
 
-class RegisterHelper: public HelperDatabase{
 
+// data[0]: A1
+// data[1]: A2
+// data[2]: B2
+class RegisterHelper : public HelperDatabase {
+public:
+	RegisterHelper() {
+		path = "RegisterHelperDatabase.txt";
+		HelperDatabase::readData(path, data);
+	}
 };
 
-class AppHelper: public HelperDatabase{
-
+// Sau khi hoan thanh ung dung, lam clip huong dan up len Youtube
+// roi doc link giong nhu nhung cai o tren
+class AppHelper : public HelperDatabase {
+public:
+	AppHelper() {
+		/*
+		path = "AppHelperDatabase.txt";
+		HelperDatabase::readData(path, data);
+		*/
+	}
 };
 
-class LocateHelper: public HelperDatabase{
-
+// Du lieu lay tu So GTVT TP HCM
+class LocateHelper : public HelperDatabase {
+public:
+	LocateHelper() {
+		path = "LocateHelperDatabase.txt";
+		HelperDatabase::readData(path, data);
+	}
 };
